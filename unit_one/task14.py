3a85b1539b3fa6632981b562217a0347aa947018
unknown <you@example.com>
2022-04-19 01:50:12 +0300

    task14

--- unit_one/task14.py
+++ unit_one/task14.py
@@ -10,22 +10,38 @@
 #Для числа 1 индексы двух ближайших чисел: 0 и 7
 #Для числа 2 индексы двух ближайших чисел: 6 и 9
 
+#mass = [1, 2, 3, 2, 0]
+#masi = []
 mass = []
-masi = []
-i2 = 0
 N = int(input("введите размер массива"))
 for i in range(N):
     mass.append(int(input("введите элемент массива")))
 print(mass)
+x = 0
 for i in mass:
     if mass.count(i) >= 2:
-        #не понимаю как вывести несколько индексов
-        while i != N:
-            i2 = (mass.index(i))
-            masi.append(i2)
-            i = i + 1
-        print(masi)
-
+        x = i
+position = 0
+start = 0
+index_for_element = []
+length = len(mass)
+while position < length:
+    i = mass.index(x, position)
+    index_for_element.append(i)
+    position = i + 1
+print(index_for_element)
+start = None
+min_ = 1000000
+for elm in index_for_element:
+    if start is None:
+        start = elm
+        continue
+    if elm - start < min_:
+        min_ = elm - start
+        first_start_index = start
+        second_start_index = elm
+print(first_start_index)
+print(second_start_index)
 
 
 
