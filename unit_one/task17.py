a5657146b4987eac8de32b85bd7b01f63249a32f
unknown <you@example.com>
2022-04-21 21:47:00 +0300

    task17

---
+++ unit_one/task17.py
@@ -0,0 +1,55 @@
+#todo: Создайте функцию compute_bill, считающаю итоговую сумму товаров в чеке.
+#Функция должна принимать 1 параметр - словарь, в котором указано количество едениц товара.
+#Цены хранятся в словаре:
+#prices = {
+#  "banana": 4,
+#  "apple": 2,
+#  "orange": 1.5,
+#  "pear": 3
+#}
+
+def compute_bill():
+
+  bill = {
+    "banana": int(input("количество бананов")),
+    "apple": int(input("количество яблок")),
+    "orange": int(input("количество апельсинов")),
+    "pear": int(input("количество груш"))
+  }
+
+  prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+  }
+
+  quantity = {k: bill[k]*prices[k] for k in bill}
+  print("всего фруктов в чеке:", quantity)
+
+  quantity_l = quantity.values()
+
+  total_price = 0
+  for i in quantity_l:
+    total_price += i
+
+  print("Итоговая сумма в чеке:", total_price)
+
+
+compute_bill()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
