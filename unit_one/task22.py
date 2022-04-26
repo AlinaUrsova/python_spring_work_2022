2308370f7d09ae58d6738cf2ddf295aa3683cada
unknown <you@example.com>
2022-04-27 02:57:58 +0300

    task22

--- unit_one/task22.py
+++ unit_one/task22.py
@@ -12,19 +12,3 @@
 # В этой задаче удобно считывать файл построчно, шифруя каждую строку в отдельности.
 #В каждой строчке содержатся различные символы. Шифровать нужно только буквы кириллицы.
 
-def cesar_encode(letter, shift):
-    result = ''
-    for letter in line:
-        if letter.isalpha():
-            number = ord(letter) + shift % 32
-            if number > 1103:
-                number -= 32
-            result += chr(number)
-        else:
-            result += letter
-    return result
-
-text = open('massage.txt', encoding='utf-8')
-text = text.readlines()
-for shift, line in enumerate(text, 1):
-    print(cesar_encode(line, shift), '\n')
