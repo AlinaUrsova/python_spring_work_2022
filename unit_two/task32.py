61735ed1887f17b5f63bed39f9a7b41c65851688
unknown <you@example.com>
2022-05-26 18:38:38 +0300

    task32

--- unit_two/task32.py
+++ unit_two/task32.py
@@ -8,4 +8,55 @@
 #Для подключения использовать пользователя и базу отличную от postgres
 
 
-см. файл create_db.py
+#см. файл create_db.py
+
+import psycopg
+
+with psycopg.connect("dbname=my_test user=my_user_test password=123") as conn:
+    with conn.cursor() as cur:
+        # создание таблиц
+        cur.execute("""CREATE TABLE "my_test_group" (
+                  "group" SERIAL PRIMARY KEY,
+                  "name" VARCHAR(100)
+                    );""")
+
+        #cur.execute("""
+            CREATE TABLE "my_test_student" (
+               "id_student" serial PRIMARY KEY,
+               "id_group" integer not null,
+               "name" varchar(100),
+               "age" int)
+            """)
+
+        # создание индекса
+        cur.execute(
+                 """CREATE INDEX "idx_my_test_student__id_group" ON "my_test_student" ("id_group");""")
+
+        #создание констрейн
+
+        #cur.execute("""
+                 ALTER TABLE "my_test_student" ADD CONSTRAINT "fk_my_test_student_id_group" FOREIGN KEY ("id_group")
+                REFERENCES "my_test_group" ("group")
+                 """)
+
+        #новая запись в таблицу
+
+        cur.execute("""
+                 INSERT INTO "my_test_group" ("group", name)
+                 VALUES (1,'group_3');
+                 """)
+
+        cur.execute("""
+                   INSERT INTO "my_test_student" (id_student, id_group, name, age )
+                   VALUES (1, 1, 'Ivan', 27);
+                   """)
+
+
+        
+        cur.execute("SELECT * FROM test")
+        cur.fetchall()
+
+        for record in cur:
+            print(record)
+
+        conn.commit()
