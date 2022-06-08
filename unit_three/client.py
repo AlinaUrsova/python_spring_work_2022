c17149a5cc04530ad5d0bb97458bb822e108ffbe
unknown <you@example.com>
2022-06-08 15:35:01 +0300

    hw

--- unit_three/client.py
+++ unit_three/client.py
@@ -1,10 +1,14 @@
 import socket
+import base64
 
 sock = socket.socket()
-sock.connect(('localhost', 1515))
-sock.send('hello, world!')
+sock.connect(('localhost', 9090))
+sock.sendall(b'hello, world!')
 
 data = sock.recv(1024)
+decode = open('ava.jpg', 'wb')
+decode.write(base64.b64decode((data)))
+decode.close()
+
 sock.close()
 
-print(data)
--- unit_three/server.py
+++ unit_three/server.py
@@ -1,15 +1,22 @@
 import socket
+import base64
 
 sock = socket.socket()
-sock.bind(('localhost', 1515))
+sock.bind(('localhost', 9090))
 sock.listen(1)
-
 conn, addr = sock.accept()
+print(addr)
+
 
 while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.send(data.upper())
+     data = conn.recv(1024)
+     #print(data)
+     if not data:
+         break
+     with open('ava.jpg', "rb") as image2string:
+         converted_string = base64.b64encode(image2string.read())
+     conn.send(converted_string)
+
+
 
 conn.close()
